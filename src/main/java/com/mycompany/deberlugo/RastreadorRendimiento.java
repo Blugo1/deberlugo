e0bbac1a4e68edf0b53da2599add4624188e0bdb
Lugo <lugobryan75@gmail.com>
2023-10-29 20:29:17 -0500

    Commit 6 escribimos la condicion del 75%

--- src/main/java/com/mycompany/deberlugo/RastreadorRendimiento.java
+++ src/main/java/com/mycompany/deberlugo/RastreadorRendimiento.java
@@ -20,6 +20,13 @@ public class RastreadorRendimiento {
         return ((double) aciertos / (double) intentos) * 100.0;
     }
 
-
-    
+    public String generarMensajeFinal() {
+        double porcentaje = calcularPorcentajeAciertos();
+        if (porcentaje < 75.0) {
+            return "Por favor pide ayuda adicional a tu instructor.";
+        } else {
+            return "Felicidades, estás listo para pasar al siguiente nivel!";
+        }
+    }
 }
+
