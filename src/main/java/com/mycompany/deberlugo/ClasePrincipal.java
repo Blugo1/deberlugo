47aa2069ae1b431401fd028c21bbf96a25ac6efe
Lugo <lugobryan75@gmail.com>
2023-10-29 20:31:51 -0500

    Commit 8 asignamos condiciones a la clase pricipal

--- src/main/java/com/mycompany/deberlugo/ClasePrincipal.java
+++ src/main/java/com/mycompany/deberlugo/ClasePrincipal.java
@@ -1,21 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.deberlugo;
+import java.util.Scanner;
 
-/**
- *
- * @author LENOVO
- */
 public class ClasePrincipal {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        ManejadorInteracciones manejadorInteracciones = new ManejadorInteracciones();
+        RastreadorRendimiento rastreadorRendimiento = new RastreadorRendimiento();
+
+        manejadorInteracciones.mostrarMensaje
+        ("Elige el nivel de dificultad (1, 2, ...): ");
+        int nivelDificultad = entrada.nextInt();
+
+        manejadorInteracciones.mostrarMensaje
+        ("Elige el tipo de problema aritmético "
+      + "(1: Suma, 2: Resta, 3: Multiplicación, 4: División, 5: Aleatorio): ");
+        int tipoProblema = entrada.nextInt();
+        
+        GeneradorProblemas generadorProblemas = new GeneradorProblemas(nivelDificultad, tipoProblema);
+
+        int totalPreguntas = 10;
+
+        for (int i = 0; i < totalPreguntas; i++) {
+            int[] operandos = generadorProblemas.generarOperandos();
+            String pregunta = generadorProblemas.generarPregunta(operandos[0], operandos[1]);
+
+            manejadorInteracciones.mostrarMensaje(pregunta);
+            int respuestaUsuario = entrada.nextInt();
+            int respuestaCorrecta = generadorProblemas.calcularRespuesta(operandos[0], operandos[1]);
+
+            if (respuestaUsuario == respuestaCorrecta) {
+                rastreadorRendimiento.registrarAcierto();
+                manejadorInteracciones.mostrarMensajePositivo();
+            } else {
+                rastreadorRendimiento.registrarFallo();
+                manejadorInteracciones.mostrarMensajeNegativo();
+            }
+        }
+
+        String mensajeFinal = rastreadorRendimiento.generarMensajeFinal();
+        manejadorInteracciones.mostrarMensaje(mensajeFinal);
+        
+        entrada.close();
     }
-    
 }
